405dae1de1b5c429649bcf99b40733bc2609ffc1
WWS Inc <info@waterloowebservices.com>
2021-09-03 15:08:53 -0400

    Revert "removed sql Express"
    This reverts commit 7a7d482653ef48275261a0a2ce816623d1cd9ea2.

--- ReadMe.md
+++ ReadMe.md
@@ -3,5 +3,3 @@ https://docs.microsoft.com/en-us/contribute/markdown-reference/
 
 
 # WWS Inc Imgur Data EF SQL Express
-[SQL Server Express LocalDB](https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-express-localdb?view=sql-server-ver15)
-1. Install LocalDB - SQL Express
---
+++ src/data/WWSInc.Imgur.Data.EF.SqlExpress/ImgurDbSqlExpress.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WWSInc.Imgur.Data.EF;
+using WWSInc.Imgur.Data.EF.Model;
+
+namespace WWSInc.Imgur.Data.SqlServer
+{
+    public class ImgurDbSqlExpress : BaseDbContext<ImgurDbSqlExpress>
+    {
+        public ImgurDbSqlExpress(DbContextOptions<ImgurDbSqlExpress> options) : base(options)
+        {
+            
+        }
+    }
+}
