1a29534bbf7fbd7cb51a05bffb751dff8460c182
WWS Inc <info@waterloowebservices.com>
2021-09-03 10:24:58 -0400

    Update Service_AlbumDataModel_Should.cs

--- src/tests/WWSInc.Imgur.Unit.Tests/ServiceTest/Service_AlbumDataModel_Should.cs
+++ src/tests/WWSInc.Imgur.Unit.Tests/ServiceTest/Service_AlbumDataModel_Should.cs
@@ -35,5 +35,55 @@ namespace Imgur_test.ServiceTest
             // Assert
             Assert.NotNull(response.Data);
         }
+
+        [Theory]
+        [InlineData(@"\sample-data\album-response.json")]
+        public void Response_AlbumDataModel_Should_Match_Title(string jsonFile)
+        {
+            // Arrange 
+            var imageIds = new List<int> { 1, 2 };
+            var deleteIdHashes = new List<int> { 1, 2 };
+            var title = "title goes here";
+            var description = "description goes here";
+            var privacy = Privacy.Hidden;
+            var coverId = 1;
+
+            var mockService = new Mock<IAlbumServices>();
+            mockService
+                .Setup(service => service.CreateAlbumAsync(imageIds, deleteIdHashes, title, description, privacy, coverId))
+                .ReturnsAsync(GetResponse(jsonFile));
+
+            // Act
+            var service = mockService.Object;
+            var response = service.CreateAlbumAsync(imageIds, deleteIdHashes, title, description, privacy, coverId).Result;
+
+            // Assert
+            Assert.Equal("Album title - 637659648000000000", response.Data.Title);
+        }
+
+        [Theory]
+        [InlineData(@"\sample-data\album-response.json")]
+        public void Response_AlbumDataModel_Should_Match_Deletehash(string jsonFile)
+        {
+            // Arrange 
+            var imageIds = new List<int> { 1, 2 };
+            var deleteIdHashes = new List<int> { 1, 2 };
+            var title = "title goes here";
+            var description = "description goes here";
+            var privacy = Privacy.Hidden;
+            var coverId = 1;
+
+            var mockService = new Mock<IAlbumServices>();
+            mockService
+                .Setup(service => service.CreateAlbumAsync(imageIds, deleteIdHashes, title, description, privacy, coverId))
+                .ReturnsAsync(GetResponse(jsonFile));
+
+            // Act
+            var service = mockService.Object;
+            var response = service.CreateAlbumAsync(imageIds, deleteIdHashes, title, description, privacy, coverId).Result;
+
+            // Assert
+            Assert.Equal("NfDgm62hz1POd8p", response.Data.DeleteHash);
+        }
     }
 }
